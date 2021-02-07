f83f94b13d6876949dbe31f56e00f576abd5d470
Andrew Dotterer <adotterer@gmail.com>
2021-02-06 22:46:18 -0800

    minor panic attack, npm didn't install ws

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const { check } = require("express-validator");
 const asyncHandler = require("express-async-handler");
-
 const { handleValidationErrors } = require("../../utils/validation");
 const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const { User } = require("../../db/models");
---
+++ backend/utils/geolocator.js
@@ -0,0 +1,29 @@
+const NodeGeocoder = require("node-geocoder");
+
+const options = {
+  provider: "google",
+
+  // Optional depending on the providers
+  // fetch: customFetchImplementation,
+  apiKey: process.env.GOOGLE_API_SECRET, // for Mapquest, OpenCage, Google Premier
+  // formatter: "object",
+};
+
+const geocoder = NodeGeocoder(options);
+
+const createLocationObj = async (location) => {
+  const res = await geocoder.geocode(location);
+  const obj = res[0];
+  const {
+    latitude,
+    longitude,
+    city,
+    administrativeLevels: { level1short },
+  } = obj;
+
+  const state = level1short;
+
+  return { latitude, longitude, city, state };
+};
+
+module.exports = { createLocationObj };
