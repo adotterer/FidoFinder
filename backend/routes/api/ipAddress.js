90d94a929470f4c096dbae438d978fbc111da963
Andrew Dotterer <adotterer@gmail.com>
2021-02-07 02:23:26 -0800

    work on ip address finder

--- backend/routes/api/ipAddress.js
+++ backend/routes/api/ipAddress.js
@@ -1,16 +1,20 @@
 const router = require("express").Router();
 const requestIp = require("request-ip");
+var geoip = require("geoip-lite");
 
 // inside middleware handler
 const ipMiddleware = function (req, res, next) {
   const clientIp = requestIp.getClientIp(req);
   req.clientIp = clientIp;
+  const geoObj = geoip.lookup(clientIp);
+  req.geoObj = geoObj;
+  console.log(geoObj);
   next();
 };
 
 router.get("/", ipMiddleware, async (req, res, next) => {
   console.log("here is clientIP --->", req.clientIp);
-  res.json("please work");
+  res.json(req.clientIp);
 });
 
 module.exports = router;
--- frontend/src/components/GreetingPage/index.js
+++ frontend/src/components/GreetingPage/index.js
@@ -1,45 +1,51 @@
 import React, { useState, useEffect } from "react";
 import { fetch } from "../../store/csrf.js";
 
-async function getIpAddress() {
-  const res = await fetch("/api/ipAddress/");
-  console.log(res.data);
-}
-
 function GreetingPage() {
-  var options = {
-    enableHighAccuracy: false,
-    timeout: 0,
-    maximumAge: 0,
+  const [ipAddress, setIpAddress] = useState();
+
+  const getIpAddress = async () => {
+    const res = await fetch("/api/ipAddress/");
+    return res.data;
   };
 
-  function success(pos) {
-    var crd = pos.coords;
+  useEffect(async () => {
+    const ip = await getIpAddress();
+    console.log(ip);
+    setIpAddress(ip);
+  }, []);
+  // var options = {
+  //   enableHighAccuracy: false,
+  //   timeout: 0,
+  //   maximumAge: 0,
+  // };
 
-    console.log("Your current position is:");
-    console.log(`Latitude : ${crd.latitude}`);
-    console.log(`Longitude: ${crd.longitude}`);
-    console.log(`More or less ${crd.accuracy} meters.`);
-  }
+  // function success(pos) {
+  //   var crd = pos.coords;
 
-  function error(err) {
-    console.warn(`ERROR(${err.code}): ${err.message}`);
-  }
+  //   console.log("Your current position is:");
+  //   console.log(`Latitude : ${crd.latitude}`);
+  //   console.log(`Longitude: ${crd.longitude}`);
+  //   console.log(`More or less ${crd.accuracy} meters.`);
+  // }
 
-  try {
-    navigator.geolocation.getCurrentPosition(success, error, options);
-  } catch (e) {
-    console.log(e);
-  }
+  // function error(err) {
+  //   console.warn(`ERROR(${err.code}): ${err.message}`);
+  // }
+
+  // try {
+  //   navigator.geolocation.getCurrentPosition(success, error, options);
+  // } catch (e) {
+  //   console.log(e);
+  // }
   // console.log(geolocation);
-  getIpAddress();
+  // getIpAddress();
 
   return (
     <div>
       <h1>GreetingPage</h1>
 
-      <div>your lat: </div>
-      <div>your lat: </div>
+      <div>your ip address: {ipAddress && ipAddress}</div>
     </div>
   );
 }
