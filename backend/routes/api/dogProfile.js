298b9901d9d144c7821f992471382065295c376f
Andrew Dotterer <adotterer@gmail.com>
2021-02-17 22:39:35 -0800

    chatRoomId... need to decide how to create a room

--- backend/routes/api/dogProfile.js
+++ backend/routes/api/dogProfile.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { Dog, DogProfile, Image, ChatRoom } = require("../../db/models");
+const { Dog, DogProfile, Image, ChatRoom, User } = require("../../db/models");
 // const { Op } = require("sequelize");
 const asyncHandler = require("express-async-handler");
 const { requireAuth } = require("../../utils/auth");
@@ -9,14 +9,17 @@ const {
   singleMulterUpload,
 } = require("../../utils/awsS3");
 
-// TODO: DELETE
+// TODO: DELETE TESTING ROUTE
 router.get("/test", async (req, res) => {
-  ChatRoom.findByPk(1, {
-    include: ["ChattingUsers"],
-  }).then((result) => {
-    console.log(result.toJSON());
-    return res.send(result.toJSON());
-  });
+  const allUserConversations = User.findByPk(1)
+    .then((user) => user.getConversations())
+    .then((convos) => console.log(convos));
+  // ChatRoom.findByPk(1, {
+  //   include: ["ChattingUsers"],
+  // }).then((result) => {
+  //   console.log(result.toJSON());
+  //   return res.send(result.toJSON());
+  // });
 });
 
 router.get(
