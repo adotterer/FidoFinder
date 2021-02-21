bf4ff04ef33386b82e43e44071ef426ac6ca9f03
Andrew Dotterer <adotterer@gmail.com>
2021-02-20 16:43:05 -0800

    live users part 1

--- backend/sockets.js
+++ backend/sockets.js
@@ -6,8 +6,29 @@ const { socketRequireAuth } = require("./utils/auth");
 const roomMap = {};
 const liveUsers = new Set();
 
-//
-io.use(socketRequireAuth).on("connection", (socket) => {
+function authorizeUser(user, chatRoomId) {
+  return db.ChatRoom.findByPk(chatRoomId, {
+    include: ["AuthorizedChatters"],
+  })
+    .then((authorizedChatters) => {
+      return authorizedChatters.toJSON().AuthorizedChatters;
+    })
+    .then((authorizedChatters) => {
+      const authorizedUser = authorizedChatters.filter((chatter) => {
+        return chatter.id === user.id;
+      });
+      return authorizedUser;
+    })
+    .then(([authorizedUser]) => {
+      return true;
+    })
+    .catch((e) => {
+      console.error(e);
+      return false;
+    });
+}
+
+io.use(socketRequireAuth).on("connection", async (socket) => {
   console.log("Connected");
 
   const {
@@ -17,31 +38,16 @@ io.use(socketRequireAuth).on("connection", (socket) => {
     },
   } = socket;
 
-  console.log(user);
-
   switch (type) {
     case "chat":
-      console.log("user", user.toJSON());
-
-      db.ChatRoom.findByPk(chatRoomId, {
-        include: ["AuthorizedChatters"],
-      })
-        .then((authorizedChatters) => {
-          return authorizedChatters.toJSON().AuthorizedChatters;
-        })
-        .then((authorizedChatters) => {
-          const authorizedUser = authorizedChatters.filter((chatter) => {
-            // console.log("chatter.id", chatter.id, "user.id", user.id);
-            return chatter.id === user.id;
-          });
-          return authorizedUser;
-        })
-        .then(([authorizedUser]) => {
-          console.log("AUTHORIZED USER", authorizedUser);
-        })
-        .catch((e) => {
-          console.error(e);
-        });
+      if (await authorizeUser(user, chatRoomId)) {
+        console.log("THIS USER IS AUTHORIZED");
+        liveUsers.add(user.toJSON());
+        console.log("LIVE USERS", liveUsers);
+      } else {
+        console.log("---- UNAUTHORIZED USER ----");
+        break;
+      }
 
       socket.on("message", (msg, chatRoomId) => {
         // find chat room by Id, to create in room map
