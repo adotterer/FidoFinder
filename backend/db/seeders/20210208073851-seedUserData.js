f0bdc2e7714d915895d6b6def4c062cbbcf014a4
Andrew Dotterer <adotterer@gmail.com>
2021-02-26 02:33:07 -0800

    begin seed tests

--- backend/db/seeders/20210208073851-seedUserData.js
+++ backend/db/seeders/20210208073851-seedUserData.js
@@ -11,7 +11,16 @@ const data = fs.readFileSync(
   }
 );
 
+const dogData = fs.readFileSync(
+  __dirname + "/resources/dogJSON.js",
+  "utf8",
+  (e) => {
+    console.error(e);
+  }
+);
+
 const jsonData = JSON.parse(data);
+const dogJsonData = JSON.parse(dogData);
 
 // ---> TODO:
 // REFACTOR THIS DATA SO I KNOW WHAT CITIES I CAN INSERT
@@ -19,6 +28,7 @@ const jsonData = JSON.parse(data);
 // COULD PASS IN THE ACTUAL USER AND IMAGE ID BY USING
 // RETURNING: TRUE IN THE OPTINOS OF BULK INSERT
 const { userArray, userDetailsArray, profileImageArray } = jsonData;
+const { dogProfileImgArr, dogArray, dogProfile } = dogJsonData;
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
@@ -30,22 +40,24 @@ module.exports = {
       console.log("SEEDED USER ARRAY");
       await queryInterface.bulkInsert("Images", profileImageArray, {});
       console.log("SEEDED IMAGES");
-      try {
-        await queryInterface.bulkInsert("UserDetails", userDetailsArray, {});
-        console.log("SEEDED USERDETAILS");
-      } catch (e) {
-        console.error(e);
-      }
+      await queryInterface.bulkInsert("UserDetails", userDetailsArray, {});
+      console.log("SEEDED USERDETAILS");
+      await queryInterface.bulkInsert("Images", dogProfileImgArr, {});
+      console.log("SEEDED DOG IMAGES");
+      await queryInterface.bulkInsert("Dogs", dogArray, {});
+      console.log("SEEDED DIG ARRAY");
+      await queryInterface.bulkInsert("DogProfiles", dogProfile, {});
+      console.log("SEEDED DOG PROFILES");
     } catch (e) {
       console.error(e);
     }
   },
 
   down: async (queryInterface, Sequelize) => {
+    queryInterface.bulkDelete("DogProfiles");
+    queryInterface.bulkDelete("Dogs");
     queryInterface.bulkDelete("UserDetails");
     queryInterface.bulkDelete("Images");
-    queryInterface.bulkDelete("Dogs");
-    queryInterface.bulkDelete("DogProfiles");
     queryInterface.bulkDelete("user_chatRooms");
     queryInterface.bulkDelete("ChatRooms");
     return queryInterface.bulkDelete("Users");
---
+++ backend/db/seeders/resources/createDogProfiles.js
@@ -0,0 +1,54 @@
+const fs = require("fs");
+const faker = require("faker");
+const bcrypt = require("bcryptjs");
+
+const dogProfileImgArr = [
+  {
+    URL:
+      "https://fido-finder-bucket.s3-us-west-1.amazonaws.com/birthdaycake.jpeg",
+  },
+  {
+    URL:
+      "https://fido-finder-bucket.s3-us-west-1.amazonaws.com/1614335044430.png",
+  },
+];
+const dogArray = [
+  {
+    name: "Penny",
+    birthday: faker.date.past(),
+    profileImageId: 1006,
+    ownerId: 1,
+    vaccination: "fully vaccinated",
+  },
+  {
+    name: "Rosie",
+    birthday: faker.date.past(),
+    profileImageId: 1007,
+    ownerId: 2,
+    vaccination: "fully vaccinated",
+  },
+];
+
+const dogProfile = [
+  {
+    dogId: 1,
+    interests: "Bork boxes",
+  },
+  {
+    dogId: 2,
+    interests: "Peanut butter",
+  },
+];
+
+try {
+  fs.writeFileSync(
+    "dogJSON.js",
+    JSON.stringify({
+      dogProfileImgArr: dogProfileImgArr,
+      dogArray: dogArray,
+      dogProfile: dogProfile,
+    })
+  );
+} catch (err) {
+  console.error(err);
+}
