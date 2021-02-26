349610757f4d991ab6e6b484b415842d5ad887c3
Andrew Dotterer <adotterer@gmail.com>
2021-02-26 08:58:50 -0800

    map user bubbles

--- frontend/src/components/GoogleMap/index.js
+++ frontend/src/components/GoogleMap/index.js
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import GoogleMapReact from "google-map-react";
 import UserList from "../UserList";
 import { Link, useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { GiSittingDog } from "react-icons/gi";
 import createChatRoomEvent from "../../utils/createChatRoomEvent";
+import "./googlemap.css";
 
-function Pin(props) {
-  const history = useHistory();
-  // console.log("pin", props);
+function UserDetailsBubble({ user }) {
   return (
-    <Link key={props.pinUser.id} to={`/user/${props.pinUser.id}`}>
-      <GiSittingDog style={{ color: "black", fontSize: "3em" }} />
-    </Link>
+    <div className="div__userDetailBubble">
+      <div className="div__bubbleUsername">{user.username}</div>
+      <Link to={`/user/${user.id}`}>
+        <div className="div__bubbleUserStatus">{user.status}</div>
+      </Link>
+    </div>
+  );
+}
+
+function Pin({ pinUser }) {
+  const history = useHistory();
+  const [showBubble, setShowBubble] = useState(false);
+
+  return (
+    <div
+      onClick={(e) => {
+        e.preventDefault();
+        if (showBubble) setShowBubble(false);
+      }}
+    >
+      {showBubble && <UserDetailsBubble user={pinUser} />}
+      {/* <Link key={pinUser.id} to={`/user/${pinUser.id}`}> */}
+      <GiSittingDog
+        onClick={(e) => {
+          // e.preventDefault();
+          setShowBubble(true);
+        }}
+        style={{ color: "black", fontSize: "3em" }}
+      />
+      {/* </Link> */}
+    </div>
   );
 }
 
@@ -21,13 +48,15 @@ function SimpleMap({ center, nearbyUsers }) {
   const sessionUser = useSelector((state) => state.session.user);
   const mappedUsers = nearbyUsers.map((user) => {
     const {
-      UserDetail: { liveLocationLat: lat, liveLocationLng: lng },
+      UserDetail: { liveLocationLat: lat, liveLocationLng: lng, status },
       username,
       id,
     } = user;
 
-    return { id, username, lat, lng };
+    return { id, username, lat, lng, status };
   });
+
+  // console.log("MAPPED USERS", mappedUsers[0]);
 
   return (
     // Important! Always set the container height explicitly
@@ -36,7 +65,7 @@ function SimpleMap({ center, nearbyUsers }) {
         bootstrapURLKeys={{ key: "AIzaSyDT_KxxLpoLwmlk3sXLpayAvW9z9_RodME" }}
         // defaultCenter={center}
         center={center}
-        defaultZoom={12}
+        defaultZoom={11}
       >
         {mappedUsers.length > 0 &&
           mappedUsers.map((user) => {
