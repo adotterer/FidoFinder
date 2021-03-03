853cbe66f420d6229f2b26e8aadb6a6a74ed9fd6
Andrew Dotterer <adotterer@gmail.com>
2021-03-02 22:20:13 -0800

    blue circle hello '

--- frontend/src/components/SplashMap/index.js
+++ frontend/src/components/SplashMap/index.js
@@ -2,6 +2,14 @@ import React, { useState } from "react";
 import GoogleMapReact from "google-map-react";
 import "./splashmap.css";
 
+function InfoCircle() {
+  return (
+    <div className="div__bluecircle">
+      <span>Hello</span>
+    </div>
+  );
+}
+
 export default function SplashMap({ center, nearbyUsers }) {
   console.log("center", center);
   return center ? (
@@ -10,7 +18,9 @@ export default function SplashMap({ center, nearbyUsers }) {
         bootstrapURLKeys={{ key: "AIzaSyDT_KxxLpoLwmlk3sXLpayAvW9z9_RodME" }}
         center={center}
         defaultZoom={11}
-      ></GoogleMapReact>
+      >
+        <InfoCircle lat={center.lat} lng={center.lng} />
+      </GoogleMapReact>
     </div>
   ) : (
     "loading....."
