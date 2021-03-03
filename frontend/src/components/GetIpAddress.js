f8ddc4bd33dbb129a6eb0644cf6abde4d8de1329
Andrew Dotterer <adotterer@gmail.com>
2021-03-03 01:39:45 -0800

    transitions from city to city

--- frontend/src/components/GetIpAddress.js
+++ frontend/src/components/GetIpAddress.js
@@ -6,16 +6,57 @@ import "./SplashMap/splashmap.css";
 function GetIpAddress() {
   const [ipAddress, setIpAddress] = useState();
 
+  const cityReelArr = [
+    // new york
+    {
+      latlng: { lat: 40.71, lng: -74 },
+      nearbyUsers: new Array(Math.ceil(Math.random() * 290)),
+    },
+    // philadelphia
+    {
+      latlng: { lat: 39.95, lng: -75.17 },
+      nearbyUsers: new Array(Math.ceil(Math.random() * 290)),
+    },
+    // balitmore
+    {
+      latlng: { lat: 39.29, lng: -76.6 },
+      nearbyUsers: new Array(Math.ceil(Math.random() * 290)),
+    },
+    // dc
+    {
+      latlng: { lat: 38.9, lng: -77.03 },
+      nearbyUsers: new Array(Math.ceil(Math.random() * 290)),
+    },
+  ];
+
   if (!ipAddress) {
     fetch("/api/ipAddress/").then((res) => setIpAddress(res.data));
   }
 
-  useEffect(async () => {
-    if (ipAddress) console.log("ipAddress, actually res.data", ipAddress);
-  }, [ipAddress]);
+  useEffect(() => {
+    // cityReelArr.forEach((city, i) => {
+    //   setTimeout(() => {
+    //     setIpAddress(city);
+    //   }, 6000 * i + 0.5);
+    // });
+
+    let i = 0;
+    setInterval(() => {
+      // setMapTransition({ opacity: 1, transition: "opacity 0.5s linear" });
+
+      if (i === cityReelArr.length) i = 0;
+      setIpAddress(cityReelArr[i]);
+      i++;
+      // setTimeout(
+      //   () =>
+      //     setMapTransition({ opacity: 0, transition: "opacity 0.5s linear" }),
+      //   2000
+      // );
+    }, 6000);
+  }, []);
 
   return ipAddress ? (
-    <div className="div__splashmap ">
+    <div className="div__splashmap">
       {ipAddress.latlng ? (
         <SplashMap
           center={ipAddress.latlng}
--- frontend/src/components/SplashMap/index.js
+++ frontend/src/components/SplashMap/index.js
@@ -1,30 +1,71 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import GoogleMapReact from "google-map-react";
 import "./splashmap.css";
 import LoginFormModal from "../LoginFormModal";
 import { Link } from "react-router-dom";
 
 function InfoCircle({ numUsers }) {
+  const [circleTransition, setCircleTransition] = useState({
+    opacity: 0,
+    transition: "opacity 1s linear",
+  });
+
+  useEffect(() => {
+    setCircleTransition({ opacity: 0, transition: "opacity 0.4s linear" });
+    setTimeout(() => {
+      setCircleTransition({ opacity: 100, transition: "opacity 0.6s linear" });
+    }, 200);
+
+    setTimeout(() => {
+      setCircleTransition({ opacity: 0, transition: "opacity 0.8s linear" });
+    }, 4860);
+  }, [numUsers]);
+
   return (
     <div className="div__bluecircle">
-      <h2>{numUsers}</h2>
-      <span>Dog owners in your area.</span>
-      <div className="div__bluecircle_loginSignUp">
-        <LoginFormModal />
-        <Link to="/signup">
-          <div>
-            <button>Sign up</button>
-          </div>
-        </Link>
+      <div className="div__transition--hidden" style={circleTransition}>
+        <h2>{numUsers}</h2>
+        <span>Dog owners in your area.</span>
+
+        <div className="div__bluecircle_loginSignUp">
+          <LoginFormModal />
+          <Link to="/signup">
+            <div>
+              <button>Sign up</button>
+            </div>
+          </Link>
+        </div>
       </div>
     </div>
   );
 }
 
 export default function SplashMap({ center, nearbyUsers }) {
-  console.log("center", center);
+  const [mapTransition, setMapTransition] = useState({
+    opacity: 0,
+    transition: "opacity .4s linear",
+  });
+
+  useEffect(() => {
+    setMapTransition({
+      opacity: "0",
+      transition: "opacity .4s linear",
+    });
+    setMapTransition({
+      opacity: "100",
+      transition: "opacity .4s linear",
+    });
+    setTimeout(
+      () =>
+        setMapTransition({
+          opacity: "0",
+          transition: "opacity .5s linear",
+        }),
+      5700
+    );
+  }, [center]);
   return center ? (
-    <div className="div__splashmap">
+    <div style={mapTransition} className="div__splashmap">
       <GoogleMapReact
         bootstrapURLKeys={{ key: "AIzaSyDT_KxxLpoLwmlk3sXLpayAvW9z9_RodME" }}
         center={center}
