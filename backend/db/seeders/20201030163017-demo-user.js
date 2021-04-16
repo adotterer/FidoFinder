ae7bd5f25a267750b0873610db292ec3253af666
Andrew Dotterer <adotterer@gmail.com>
2021-04-16 07:56:21 -0700

    downgrade fix

---
+++ backend/db/seeders/20201030163017-demo-user.js
@@ -0,0 +1,57 @@
+"use strict";
+const faker = require("faker");
+const bcrypt = require("bcryptjs");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          email: "demo@user.io",
+          username: "Demo-lition",
+          hashedPassword: bcrypt.hashSync("password"),
+          firstName: faker.name.firstName(),
+          lastName: faker.name.lastName(),
+          phoneNumber: faker.phone.phoneNumber(),
+          role: "dog_owner",
+        },
+        {
+          email: "onika@pink.com",
+          username: "Barbie",
+          hashedPassword: bcrypt.hashSync("password"),
+          firstName: "Onika",
+          lastName: "Minaj",
+          phoneNumber: faker.phone.phoneNumber(),
+          role: "dog_owner",
+        },
+        {
+          email: faker.internet.email(),
+          username: "FakeUser1",
+          hashedPassword: bcrypt.hashSync(faker.internet.password()),
+          firstName: faker.name.firstName(),
+          lastName: faker.name.lastName(),
+          phoneNumber: faker.phone.phoneNumber(),
+          role: "dog_owner",
+        },
+        {
+          email: faker.internet.email(),
+          username: "FakeUser2",
+          hashedPassword: bcrypt.hashSync(faker.internet.password()),
+          firstName: faker.name.firstName(),
+          lastName: faker.name.lastName(),
+          phoneNumber: faker.phone.phoneNumber(),
+          role: "dog_owner",
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(
+      "Users"
+    );
+  },
+};
