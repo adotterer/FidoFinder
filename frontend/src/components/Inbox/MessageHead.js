61524c0801203a8d36f748300432dbccee356fcb
Andrew Dotterer <adotterer@gmail.com>
2021-07-26 16:50:45 -0700

    inbox links to chat rooms

--- frontend/src/components/Inbox/MessageHead.js
+++ frontend/src/components/Inbox/MessageHead.js
@@ -1,15 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function MessageHead([topMessage]) {
-  console.log(topMessage, "top message");
   const [otherUsers] = topMessage.otherUsers;
-  console.log(otherUsers);
   return (
-    <div key={"inbox#" + topMessage.chatRoomId}>
-      <h4>Your conversation with {otherUsers.username}:</h4>
-      <div>
-        <span>{topMessage.username}: </span>
-        {topMessage.message}
+    <Link to={`/chatroom/${topMessage.chatRoomId}`}>
+      <div key={"inbox#" + topMessage.chatRoomId}>
+        <h4>Your conversation with {otherUsers.username}:</h4>
+        <div>
+          <span>{topMessage.username}: </span>
+          {topMessage.message}
+        </div>
+        <hr />
       </div>
-      <hr />
-    </div>
+    </Link>
   );
 }
