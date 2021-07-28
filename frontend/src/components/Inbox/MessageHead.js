e4777bcfaaaa0b635c20ffa61992d4e7cc83724a
Andrew Dotterer <adotterer@gmail.com>
2021-07-27 18:36:24 -0700

    inbox styling

--- frontend/src/components/Inbox/MessageHead.js
+++ frontend/src/components/Inbox/MessageHead.js
@@ -1,16 +1,19 @@
 import { Link } from "react-router-dom";
+import "./inbox.css";
 
 export default function MessageHead([topMessage]) {
   const [otherUsers] = topMessage.otherUsers;
   return (
     <Link to={`/chatroom/${topMessage.chatRoomId}`}>
-      <div key={"inbox#" + topMessage.chatRoomId}>
+      <div
+        className="inbox__messageHead"
+        key={"inbox#" + topMessage.chatRoomId}
+      >
         <h4>Your conversation with {otherUsers.username}:</h4>
         <div>
           <span>{topMessage.username}: </span>
           {topMessage.message}
         </div>
-        <hr />
       </div>
     </Link>
   );
